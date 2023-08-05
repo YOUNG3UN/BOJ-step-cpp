ace765d11739645025d3bfdab8b669f60511d5e3
YoungEun <y_e_99@ewhain.net>
2023-08-05 20:34:05 +0900

    complete: 5_string

--- 5_string/11718.cpp
+++ 5_string/11718.cpp
@@ -1,3 +1,18 @@
-//
-// Created by YE on 2023-07-06.
-//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string s;
+
+    while (1) {
+        getline(cin, s);
+        if (s == "")    //공백이 입력되면 종료
+            break;
+        cout << s << '\n';
+    }
+
+
+    return 0;
+}
--- 5_string/2908.cpp
+++ 5_string/2908.cpp
@@ -1,3 +1,17 @@
-//
-// Created by YE on 2023-07-06.
-//
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    string a, b;
+
+    cin >> a >> b;
+
+    reverse(a.begin(), a.end());
+    reverse(b.begin(), b.end());
+    cout << max(a, b);
+
+    return 0;
+}
